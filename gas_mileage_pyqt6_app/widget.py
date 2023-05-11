3cc7c6af84d4da078ee8a819c9434e055d2a2ac7
nathancaran <caran494@hsd.k12.or.us>
2023-05-11 11:59:19 -0700

    Adding widgets to V2 app

--- gas_mileage_pyqt6_app/widget.py
+++ gas_mileage_pyqt6_app/widget.py
@@ -1,67 +1,48 @@
 import sys
 from tkinter.ttk import Widget
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QHBoxLayout,
-                             QVBoxLayout,QLabel,QPushButton,QSpinBox,)
-from PyQt6.QtGui import QPalette, QColor
+                             QVBoxLayout,QLabel,QPushButton,QSpinBox,QLineEdit,QDoubleSpinBox,
+                             QCheckBox,QComboBox,QListWidget)
+from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Gas Mileage Calculator")
 
+        # Main layout ####################
         main_layout = QHBoxLayout()
 
+        # Left and right panes #############
         left_pane = QVBoxLayout()
-        
-        # Add layouts and widgets to left pane
-        hbox1 = QHBoxLayout()
-        current_label = QLabel("Current Mileage")
-        mileage_sb = QSpinBox()
-        mileage_sb.setMinimum(0)
-        mileage_sb.setSuffix("  Miles")
-        hbox1.addWidget(current_label)
-        hbox1.addWidget(mileage_sb)
-        left_pane.addLayout(hbox1)
-
-        main_layout.addLayout(left_pane)
-
-        # Add previous mileage box
         right_pane = QVBoxLayout()
-        hbox2 = QHBoxLayout()
-        current_label = QLabel("Previous Mileage")
-        prev_mileage_sb = QSpinBox()
-        prev_mileage_sb.setMinimum(0)
-        prev_mileage_sb.setSuffix("  Miles")
-        hbox2.addWidget(current_label)
-        hbox2.addWidget(prev_mileage_sb)
-        hbox2.addWidget(current_label)
-        right_pane.addLayout(hbox2)
+        
+        title_label = QLabel("Gas Mileage Calculator")
+        
+        # Set fonts #################
+        h1_font = title_label.font()
+        h1_font.setPointSize(30)
+        title_label.setFont(h1_font)
+
+        # Results label ###################
+        results_label = QLabel("MPG")
+        h2_font = results_label.font()
+        h2_font.setPointSize(26)
+        results_label.setFont(h2_font)
 
         
+        # Add left pane widgets
+        left_pane.addWidget(title_label)
+
+        # Add right pane widgets
+        right_pane.addWidget(results_label)
+
+        # Add the two panes to the layout
         main_layout.addLayout(left_pane)
         main_layout.addLayout(right_pane)
-        
-        
-        # Add gallons filled box
-        hbox3 = QHBoxLayout()
-        current_label = QLabel("Gallons Filled")
-        gallons_sb = QSpinBox()
-        gallons_sb.setMinimum(0)
-        gallons_sb.setSuffix("  Gallons")
-        hbox3.addWidget(current_label)
-        hbox3.addWidget(gallons_sb)
-        hbox3.addWidget(current_label)
-        hbox3.addWidget(current_label)
-        left_pane.addLayout(hbox3)
-
-        
-        widget = QWidget()
-        widget.setLayout(main_layout)
-        self.setCentralWidget(widget)
-
-
+                                 
 
 app = QApplication(sys.argv)
 
